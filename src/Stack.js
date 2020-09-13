3f4cdbd1d11f18f090bbbdb51791deffc2a2eaca
Bera <berakoc97@gmail.com>
2020-09-14 00:34:48 +0300

    🌌 Project configured. ➕ 📦 NEW: Stack added.

---
+++ src/Stack.js
@@ -0,0 +1,43 @@
+// @flow
+
+export default class Stack<T> {
+    constructor(items:T[]=[]) {
+        const self = this
+        this.stack = {
+            itemsInternal: items,
+            itemsListener: function () {
+                self.length = this.itemsInternal.length
+            },
+            set items(items: T[]) {
+                this.itemsInternal = items
+                this.itemsListener()
+            },
+            get items(): T[] {
+                return this.itemsInternal
+            }
+        }
+        this.length = items.length
+    }
+
+    push(item: T | T[]): void {
+        this.stack.items = this.stack.items.concat(item)
+    }
+
+    pop(): T {
+        const poppedItem = this.stack.items[this.length - 1]
+        this.stack.items = this.stack.items.slice(0, -1)
+        return poppedItem
+    }
+
+    peek(): T {
+        return this.stack.items[this.length - 1]
+    }
+
+    isEmpty(): boolean {
+        return this.length === 0
+    }
+
+    empty(): void {
+        this.stack.items = []
+    }
+}
---
+++ test/Stack.test.js
@@ -0,0 +1,31 @@
+// @flow
+import Stack from '../src/Stack'
+
+describe('Test Suite For Stack', () => {
+    let stackObject: Stack<number>
+
+    beforeEach(() => {
+        stackObject = new Stack<number>()
+    })
+
+    it('should return true since stack is empty', () => {
+        expect(stackObject.isEmpty()).toBe(true)
+    })
+
+    it('should pop an element and return it', () => {
+        stackObject.push([1, 2, 3])
+        expect(stackObject.pop()).toBe(3)
+    })
+
+    it('should return 4 as the peek of the stack', () => {
+        stackObject.push([1,2,3,4])
+        expect(stackObject.peek()).toBe(4)
+    })
+
+    it('should empty the current stack', () => {
+        stackObject.push([1, 2])
+        stackObject.empty()
+        expect(stackObject.isEmpty()).toBe(true)
+    })
+
+})
